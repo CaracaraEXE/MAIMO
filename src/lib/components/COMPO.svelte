41b0af06f31f80013e2d61776b6a21d6aae1b207
CaracaraEXE <117363174+CaracaraEXE@users.noreply.github.com>
2023-06-16 12:41:16 -0300

    newchanges

---
+++ src/lib/components/COMPO.svelte
@@ -0,0 +1,6 @@
+<script></script>
+
+<h1>Hammon</h1>
+<p>Jamón Iberico is UMAMI!!!!!!</p>
+
+<style></style>
---
+++ src/lib/components/contador.svelte
@@ -0,0 +1,11 @@
+<script>
+    let contador = 0;
+
+    function ejecutar() {
+        contador++;
+    }
+</script>
+
+<p>Contador: {contador}</p>
+
+<button on:click={ejecutar} class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Contar</button>
